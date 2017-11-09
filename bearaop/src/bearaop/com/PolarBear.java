4b3db2d10988316dffc080bebd7c8f5d35a44c8c
nicobrix1 <secretaria1998@yahoo.com>
2017-11-09 17:45:33 +0800

    Polar Bear Sleep

--- bearaop/src/bearaop/com/PolarBear.java
+++ bearaop/src/bearaop/com/PolarBear.java
@@ -12,6 +12,7 @@ public class PolarBear extends BearServiceLive{
 	public void sleep() {
 		// TODO Auto-generated method stub
 		System.out.println("  Polar Bear sleeps in cold bed.");
+		System.out.println(" Go to sleep Polar Bear.");
 	}
 	@Override
 	public void bath() {
