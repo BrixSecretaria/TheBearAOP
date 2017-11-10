645e7957d24596a997aeabdd2c2c6a4d72cdf9b4
Brix Nicholson Secretaria <secretaria1998@yahoo.com>
2017-11-10 15:29:36 +0800

    Trying out rebase with Polar Bear

--- bearaop/src/bearaop/com/PolarBear.java
+++ bearaop/src/bearaop/com/PolarBear.java
@@ -13,6 +13,7 @@ public class PolarBear extends BearServiceLive{
 		// TODO Auto-generated method stub
 		System.out.println("  Polar Bear sleeps in cold bed.");
 		System.out.println(" Go to sleep Polar Bear.");
+		System.out.println(" Trying out rebase.");
 	}
 	@Override
 	public void bath() {
