f77a569c65b4b65410c814fed8d5cbc8b7f08a37
nicobrix1 <secretaria1998@yahoo.com>
2017-11-10 15:32:09 +0800

    Revert "Trying out rebase with Polar Bear"
    This reverts commit 645e7957d24596a997aeabdd2c2c6a4d72cdf9b4.

--- bearaop/src/bearaop/com/PolarBear.java
+++ bearaop/src/bearaop/com/PolarBear.java
@@ -13,7 +13,6 @@ public class PolarBear extends BearServiceLive{
 		// TODO Auto-generated method stub
 		System.out.println("  Polar Bear sleeps in cold bed.");
 		System.out.println(" Go to sleep Polar Bear.");
-		System.out.println(" Trying out rebase.");
 	}
 	@Override
 	public void bath() {
